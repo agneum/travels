45f50c16a51e44a3157d5f8ec548e11017792221
Artem Kartasov <agneum@gmail.com>
2017-08-20 19:02:49 +0700

    travels. Unzip an archive

--- importer/importer.go
+++ importer/importer.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
 	"strings"
 
 	mgo "gopkg.in/mgo.v2"
 )
 
-const dataPath = "/tmp/data"
+const zipPath = "/tmp/data"
+const dataPath = "../tmp"
 
 func main() {
 	session, err := mgo.Dial("localhost:27017")
@@ -22,8 +26,49 @@ func main() {
 
 	session.SetMode(mgo.Monotonic, true)
 
-	importData(session)
+	err = unzip(fmt.Sprintf("%s/%s", zipPath, "data.zip"), "../tmp")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	importData(session)
+}
+
+func unzip(archive, target string) error {
+	reader, err := zip.OpenReader(archive)
+	if err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(target, 0755); err != nil {
+		return err
+	}
+
+	for _, file := range reader.File {
+		path := filepath.Join(target, file.Name)
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+			continue
+		}
+
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+		defer targetFile.Close()
+
+		if _, err := io.Copy(targetFile, fileReader); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func importData(s *mgo.Session) {
